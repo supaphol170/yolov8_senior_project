b2ab2793825cc9c002c9ee5946b2e1a7dff4c970
supaphol kodtom <124768326+supaphol170@users.noreply.github.com>
2023-10-30 18:03:00 +0700

    Update app.py

--- website/app.py
+++ website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, send_from_directory
+from flask import Flask, render_template, request, Response, redirect, url_for
 from ultralytics import YOLO
 import cv2
 import numpy as np
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join('static', 'runs')
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
 model = YOLO("best.pt") # Model as train own dataset for predict
 ALLOWED_EXTENSIONS = {'mp4', 'pdf', 'png', 'jpg', 'jpeg', 'gif'} #for protect that user insert input on website 
 
@@ -14,7 +15,8 @@ ALLOWED_EXTENSIONS = {'mp4', 'pdf', 'png', 'jpg', 'jpeg', 'gif'} #for protect th
 def detect():
     if request.method == "POST":
         uploaded_file = request.files["file"]
-        if uploaded_file:
+        type_input = uploaded_file.mimetype.split('/')[1]
+        if uploaded_file and type_input == 'jpeg':
             # Read the uploaded image
             image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
             # Perform object detection
@@ -26,7 +28,23 @@ def detect():
                 keep_path = app.config['UPLOAD_FOLDER']+"\\"+path_images #for combine variable path_images with \ and app.config['UPLOAD_FOLDER'] to full path image save
                 final_find = os.path.join(keep_path, in_pathimages) #for open keep_path to open image on web
                 return render_template('show.html', images=final_find) # show find html name show with image
-        #this line use for render_template('error.html') when can't process line 14-22 
+        elif uploaded_file and type_input == 'mp4':
+            filename = secure_filename(uploaded_file.filename) #for save input video ti this variable and make sure not another type input from user
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  #for save input in lines 31 to path
+            # Perform object detection
+            results = model.predict(filename, save_txt=True, save=True, project="C:\\Users\\Babe\\Downloads\\senior project\\environment\\test\\static\\runs")
+            #access attributes line 35
+            for video_path in results:
+                path_vieo = video_path.save_dir.split("\\")[9] # For split path by \\ to list and go to index 9 for use word "predict1-.."
+                in_pathvideo = video_path.path #for query name image as save in this case is name "image0.jpg"
+                keep_path = app.config['UPLOAD_FOLDER']+"\\"+path_vieo #for combine variable path_images with \ and app.config['UPLOAD_FOLDER'] to full path image save
+                final_find = os.path.join(keep_path, in_pathvideo) #for open keep_path to open image on web
+                display_video(results)
+                return render_template('show.html', filename=results)
+        
+@app.route('/display/<filename>')
+def display_video(filename):
+    return redirect(url_for('static', filename='runs/' + filename), code=301)
 
 @app.route('/')
 def index():
