22d90c39e1275a3de3f968fbc0c8e2de23ceb5d8
supaphol kodtom <124768326+supaphol170@users.noreply.github.com>
2024-03-10 13:32:41 +0700

    Update esp32-devkitv1.ino

--- website/esp32-devkitv1.ino
+++ website/esp32-devkitv1.ino
@@ -1,15 +1,24 @@
 #include <WiFi.h>
 #include <WebSocketsClient.h>
+#include <ArduinoJson.h>
 
-const char* ssid = "Evefy@0@";
-const char* password = "63070111";
-const char* server = "192.168.34.157";
+const char* ssid = "realme 6 pro";
+const char* password = "babe1234";
+const char* server = "127.0.0.1";
 const int port = 8887;
+const int relay_IN1 = 26;
+const int relay_IN2 = 32;
+const char* state_lamp;
 
 WebSocketsClient webSocket;
 
 void setup() {
     Serial.begin(115200);
+    pinMode(relay_IN1, OUTPUT);
+    pinMode(relay_IN2, OUTPUT);
+    // Set the initial state of the lamp (closed)
+    digitalWrite(relay_IN1, HIGH);
+    digitalWrite(relay_IN2, HIGH);
 
     // Connect to Wi-Fi
     WiFi.begin(ssid, password);
@@ -36,11 +45,104 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             webSocket.begin(server, port, "/");
             break;
         case WStype_CONNECTED:
+          //send data to node.js
+            webSocket.sendTXT("ESP32 Devkitv1 can connected");
             Serial.println("WebSocket Connected!");
             break;
         case WStype_TEXT:
             Serial.printf("Received message: %s\n", payload);
-            // Handle the received message as needed
-            break;
+            // if right it's in 2, if left it's in 1
+            // Parse the JSON payload
+            DynamicJsonDocument doc(256); // Adjust the size based on your JSON structure
+            DeserializationError error = deserializeJson(doc, payload, length);
+            if (error) {
+                Serial.print("Failed to parse JSON: ");
+                Serial.println(error.c_str());
+                return;
+            }
+            // Extract data from the JSON document
+            const char* dataValue = doc["data"];
+            state_lamp = dataValue;
+            //lamp1
+            if (strcmp(dataValue, "sleep_left") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN1, LOW);
+            }
+            else if (strcmp(dataValue, "read_left") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN1, LOW);
+                delay(50);
+                digitalWrite(relay_IN1, HIGH);
+                delay(50);
+                digitalWrite(relay_IN1, LOW);
+            }
+            else if (strcmp(dataValue, "another_left") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN1, LOW);
+                delay(50);
+                digitalWrite(relay_IN1, HIGH);
+                delay(50);
+                digitalWrite(relay_IN1, LOW);
+                delay(50);
+                digitalWrite(relay_IN1, HIGH);
+                delay(50);
+                digitalWrite(relay_IN1, LOW);
+            }
+            else if (strcmp(dataValue, "close_left") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN1, HIGH);
+            }
+            //lamp2
+            else if (strcmp(dataValue, "sleep_right") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN2, LOW);
+            }
+            else if (strcmp(dataValue, "read_right") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN2, LOW);
+                delay(50);
+                digitalWrite(relay_IN2, HIGH);
+                delay(50);
+                digitalWrite(relay_IN2, LOW);
+            }
+            else if (strcmp(dataValue, "another_right") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN2, LOW);
+                delay(50);
+                digitalWrite(relay_IN2, HIGH);
+                delay(50);
+                digitalWrite(relay_IN2, LOW);
+                delay(50);
+                digitalWrite(relay_IN2, HIGH);
+                delay(50);
+                digitalWrite(relay_IN2, LOW);
+            }
+            else if (strcmp(dataValue, "close_right") == 0) {
+                Serial.printf("Received data is : %s\n", dataValue);
+                delay(50);
+                digitalWrite(relay_IN2, HIGH);
+            } 
+            else{
+              // for send re-connect status back to flask
+              Serial.print("Received message from server: ");
+              Serial.println(dataValue);
+              // Check if the received message is a reconnection request
+              if (strcmp(dataValue, "Reconnect") == 0) {
+                // Send a response back to the server
+                const char* responseMessage = "ESP32 Devkitv1 can connected";
+                // Send the formatted date and time to the Node.js server
+                // Return the result to the calling code
+                webSocket.sendTXT(responseMessage);
+              }
+              
+            }
+          break;
     }
 }
